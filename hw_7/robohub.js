84cfbc9dc5080e772b690133fc6ea0d6c14c1a7b
Inna Ivchenko <i_lincoln_a@list.ru>
2018-12-08 21:07:29 +0200

    added solution for level#7

--- hw_7/robohub.js
+++ hw_7/robohub.js
@@ -45,39 +45,70 @@ for(i=0; i<6; i++){east();}
 
 
 //-------6-LEVEL-6-------//
-while(isFree('south')){
+while (isFree('south')) {
   south();
 }
-for(i=0; i<6; i++){
-  if(isFree('east')){
+for (i = 0; i < 6; i++) {
+  if (isFree('east')) {
     east();
   }
-  else if(isFree('north')){
-    while(isFree('north')){
+  else if (isFree('north')) {
+    while (isFree('north')) {
       north();
     }
     east();
-    do{
+    do {
       south();
     }
     while (isFree('south'));
   }
 }
-for(i=0; i<4; i++){
+for (i = 0; i < 4; i++) {
   south();
 }
-do{
+do {
   west();
 }
-while(isFree('west'));
-for(i=0; i<3; i++){
-  if(isFree('south')){
+while (isFree('west'));
+for (i = 0; i < 3; i++) {
+  if (isFree('south')) {
     south();
   }
-  while(isFree('east')){
+  while (isFree('east')) {
     east();
   }
 }
 
 
 //-------7-LEVEL-7-------//
+while (isFree('south')) {
+  south();
+}
+for (i = 0; i < 4; i++) {
+  east();
+}
+for (i = 0; i < 11; i++) {
+  north();
+}
+for (i = 0; i < 3; i++) {
+  east();
+  if (isFree('south')) {
+    do {
+      south();
+    }
+    while (isFree('south'));
+  }
+}
+west();
+while (isFree('south')) {
+  south();
+}
+while (isFree('east')) {
+  east();
+}
+for (i = 0; i < 9; i++) {
+  south();
+  if (isFree('east')) {
+    east();
+  }
+}
