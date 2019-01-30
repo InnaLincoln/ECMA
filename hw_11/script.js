035b8b85056e3ceab15fcd16d0b6c5a0091c8b01
Inna Ivchenko <i_lincoln_a@list.ru>
2019-01-30 19:21:02 +0200

    added interaction with local storage

--- hw_11/script.js
+++ hw_11/script.js
@@ -12,6 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
 class TaskList {
   constructor(value = '') {
     this._value = value;
+    this.id = -3;
+    this.mask = 'task_';
   }
 
   /**
@@ -21,26 +23,36 @@ class TaskList {
     if (this._value.length < 3) {
       throw new Error('Too few chapters');
     }
-
     this.ul = list;
     this.li = document.createElement("li");
+    this.li.setAttribute('data-item', this.mask + this.id);
     this.li.appendChild(document.createTextNode("" + this._value));
     this.ul.appendChild(this.li);
+    for (Attr in this.ul.children) {
+      let newId = this.li.getAttribute('data-item');
+      if (newId > this.id) {
+        this.id = newId;
+      }
+      this.id++;
+    }
+    localStorage.setItem(this.mask + this.id, this._value)
   }
 }
+
 
 class Task extends TaskList {
   newItem(list) {
     super.newItem(list);
-    this.deleteButton = document.createElement('span');
-    this.deleteButton.className = "delete";
-    this.deleteButton.innerHTML = " &#10005";
     this.editButton = document.createElement('span');
     this.editButton.className = "edit";
     this.editButton.innerHTML = " &#9998";
+    this.deleteButton = document.createElement('span');
+    this.deleteButton.className = "delete";
+    this.deleteButton.innerHTML = " &#10005";
     //   this.allTasks = document.querySelector('.all_tasks');
-    this.removeItem();
     this.checkedItem();
+    this.removeItem();
+
 //    this.countTasks();
   }
 
@@ -49,8 +61,9 @@ class Task extends TaskList {
     this.li.addEventListener('click', function () {
       this.classList.toggle('checked');
     });
-    this.li.appendChild(this.deleteButton);
     this.li.appendChild(this.editButton);
+    this.li.appendChild(this.deleteButton);
+
   }
 
   removeItem() {
