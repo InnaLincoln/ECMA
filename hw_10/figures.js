377e0ad5a6cec21f6cda84e10c67b529cf79b101
Inna Ivchenko <i_lincoln_a@list.ru>
2019-02-06 19:10:36 +0200

    added hw with figures, but not full

---
+++ hw_10/figures.js
@@ -0,0 +1,137 @@
+class Point {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  /**
+   *
+   * @returns {*}
+   */
+  get x() {
+    return this._x;
+  }
+
+  /**
+   *
+   * @returns {*}
+   */
+  get y() {
+    return this._y;
+  }
+
+  /**
+   *
+   * @param x1
+   * @param y1
+   * @returns {Point}
+   */
+  getPointAtOffset(x1, y1) {
+    return new Point(this.x + x1, this.y + y1);
+  }
+
+  /**
+   *
+   * @param {Point} point
+   * @returns {number}
+   */
+  getDistance(point) {
+    return Math.sqrt(((this.x - point.x) ** 2) + ((this.y - point.y) ** 2));
+  }
+}
+
+class Shape {
+  /**
+   *
+   * @param {Point} center
+   */
+  constructor(center) {
+    this._center = center;
+  }
+}
+
+class Polygon extends Shape {
+  constructor(center, points) {
+    super(center);
+    this._points = points;
+  }
+}
+
+class Rectangle extends Polygon {
+  /**
+   *
+   * @param {Point} center
+   * @param {Rectangle} width
+   * @param {Rectangle} height
+   */
+  constructor(center, width, height) {
+    super(center);
+    this.width = width;
+    this.height = height;
+  }
+
+  /**
+   *
+   * @returns {number}
+   */
+  getPerimeter() {
+    return 2 * (this.width + this.height);
+  }
+
+  /**
+   *
+   * @returns {number}
+   */
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  /**
+   *
+   * @param {Point} center
+   * @param {Rectangle} width
+   */
+  constructor(center, width) {
+    super(center, width, width);
+    this.width = width;
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+
+  getArea() {
+    return this.width ** 2;
+  }
+}
+
+class Circle extends Shape {
+  /**
+   *
+   * @param {Point} center
+   * @param {Point} radius
+   */
+  constructor(center, radius) {
+    super(radius);
+    this._radius = radius;
+  }
+
+  getPerimeter() {
+    return 2 * Math.PI * this._radius;
+  }
+
+  getArea() {
+    return Math.PI * (this._radius ** 2);
+  }
+}
+
+Shapes = {
+  Shape: Shape,
+  Polygon: Polygon,
+  Rectangle: Rectangle,
+  Square: Square,
+  Circle: Circle,
+  Point: Point
+};
